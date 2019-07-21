b5af5ccce15a36b4160c7286a34f5b22180ec477
shuiwuchana <2511702063@qq.com>
2019-07-21 16:03:41 +0800

    增加了ADC采集电流

--- software/rt-thread-master/bsp/stm32f40x/drivers/drv_adc.h
+++ software/rt-thread-master/bsp/stm32f40x/drivers/drv_adc.h
@@ -10,9 +10,11 @@ int adc_init(void);
 
 uint16 get_adc(uint8 ch);  //get adc通道值
 
+uint16 get_adc3(uint8 ch); //get adc3通道值
+
 float get_voltage_value(void); //get 电压值
 
-
+float get_current_value(void);// get电流值 
 
 #endif
 
